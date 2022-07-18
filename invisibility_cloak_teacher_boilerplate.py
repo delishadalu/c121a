46fd9d3086b253344a9021daa95cadd50a100e28
delishadalu <73183936+delishadalu@users.noreply.github.com>
2022-07-18 11:13:29 +0530

    Add files via upload

---
+++ invisibility_cloak_teacher_boilerplate.py
@@ -0,0 +1,43 @@
+import cv2
+import time
+import numpy as np
+
+#To save the output in a file output.avi
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output_file = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
+#Starting the webcam
+cap = cv2.VideoCapture(0)
+
+#Allowing the webcam to start by making the code sleep for 2 seconds
+time.sleep(2)
+bg = 0
+
+#Capturing background for 60 frames
+for i in range(60):
+    ret, bg = cap.read()
+#Flipping the background
+bg = np.flip(bg, axis=1)
+
+#Reading the captured frame until the camera is open
+while (cap.isOpened()):
+    ret, img = cap.read()
+    if not ret:
+        break
+    #Flipping the image for consistency
+    img = np.flip(img, axis=1)
+
+
+
+    #Generating the final output
+    final_output = img
+    output_file.write(img)
+    #Displaying the output to the user
+    cv2.imshow("magic", final_output)
+    cv2.waitKey(1)
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
