6f83ea2e5a5d00f4ee7cf61f4da8533645274829
delishadalu <73183936+delishadalu@users.noreply.github.com>
2022-08-23 20:29:11 +0530

    Update invisibility_cloak_teacher_boilerplate.py

--- invisibility_cloak_teacher_boilerplate.py
+++ invisibility_cloak_teacher_boilerplate.py
@@ -2,9 +2,7 @@ import cv2
 import time
 import numpy as np
 
-#To save the output in a file output.avi
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output_file = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
 
 #Starting the webcam
 cap = cv2.VideoCapture(0)
